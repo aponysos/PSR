bff8931d7509844d63fb8a73ebdc4a882e9368a4
aponysos <aponysos@163.com>
2018-02-26 17:34:56 +0800

    项目进展跟踪表->项目进展关键节点

--- psr.py
+++ psr.py
@@ -1,7 +1,9 @@
 #! python3.6
 
-import logging
 import sys
+import logging
+import xlrd
+import csv
 
 LOGGING_FORMAT =        '[%(levelname)5s] %(asctime)s %(msecs)3d ' + \
                         '{%(filename)s:%(lineno)4d} %(message)s'
@@ -17,4 +19,53 @@ logging.basicConfig(
 logging.info('Start PSR logging ...')
 logging.info('sys.argv: %s', sys.argv)
 logging.info('sys.path: %s', sys.path)
+
+mapper = [ \
+    1, 2, 3, 4, 5, 31, \
+    34, 6, 7, 8, 9, 0, 0, 0, 0, \
+    42, 62, 66, \
+    75, 76, 78, 82, 84, 10, \
+    89, 93, 96, 115, 119, 122, \
+    105, 109, 131, 135
+    ]
+
+class Project():
+    def __init__(self, sheet):
+        self.sheet = sheet
+        self.rows = []
+
+    def cell_value(self, row, col):
+        value = self.sheet.cell(row, col).value
+        logging.info('(%s, %s) %s', row, col, value)
+        return value
+
+    def parse(self):
+        for i in range(2, sheet.nrows):
+            row = []
+            for j in mapper:
+                row.append(self.cell_value(i, j))
+            self.rows.append(row)
+
+INPUT_FILE_NAME = 'input/input.xlsx'
+input = xlrd.open_workbook(INPUT_FILE_NAME)
+logging.info('input file %s opend', INPUT_FILE_NAME)
+
+sheet = input.sheets()[0]
+for i in range(1, sheet.ncols):
+    logging.info('%s : %s', i, sheet.cell(1, i))
+
+for j in mapper:
+    logging.info('%s : %s', j, sheet.cell(1, j))
+
+logging.info('-------- Begin reading sheet %s', sheet.name)
+p = Project(sheet)
+p.parse()
+logging.info('-------- End reading sheet %s', sheet.name)
+
+OUTPUT_FILE_NAME = 'output/output.csv'
+with open(OUTPUT_FILE_NAME, 'w', newline='') as output:
+    writer = csv.writer(output)
+    for r in p.rows:
+        writer.writerow(r)
+
 logging.info('End PSR logging ...')
