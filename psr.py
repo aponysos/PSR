7492e197a5dcde9e05e43be8ac4fe79dd6239c57
aponysos <aponysos@163.com>
2018-10-17 12:47:17 +0800

    init mapper from column names

--- psr.py
+++ psr.py
@@ -5,7 +5,7 @@ import logging
 import xlrd
 import csv
 
-LOGGING_FORMAT =        '[%(levelname)5s] %(asctime)s %(msecs)3d ' + \
+LOGGING_FORMAT =        '[%(levelname)7s] %(asctime)s %(msecs)3d ' + \
                         '{%(filename)s:%(lineno)4d} %(message)s'
 LOGGING_DATE_FORMAT =   '%Y-%m-%d %H:%M:%S'
 
@@ -20,16 +20,15 @@ logging.info('Start PSR logging ...')
 logging.info('sys.argv: %s', sys.argv)
 logging.info('sys.path: %s', sys.path)
 
-mapper0 = [ \
-    1, 2, 3, 4, 5, 31, \
-    34, 6, 7, 8, 9, 0, 0, 0, 0, \
-    42, 62, 66, \
-    75, 76, 78, 82, 84, 10, \
-    89, 93, 96, 115, 119, 122, \
-    105, 109, 131, 135
+COL_NAMES = [ \
+    '项目类型', '项目编码', '项目名称', '专业类别', '项目年份', '项目经理', \
+    '可研批复完成时间', '计划采购申请完成时间', '计划合同签订完成时间', '计划订单完成时间', '计划到货完成时间', \
+    '采购申请完成时间', '合同签订完成时间', '订单完成时间', '到货完成时间', \
+    '设计委托完成时间', '计划设计批复完成时间', '设计批复完成时间', \
+    '工程实施发起时间', '计划要求完工时间', '工程实施完成时间', '计划割接上线时间', '割接上线完成时间', '计划投入试运行时间', \
+    '初步验收报告完成时间', '计划初步验收时间', '初步验收批复完成时间', '竣工验收报告完成时间', '计划竣工验收时间', '竣工验收批复完成时间', \
+    '结算审计申请完成时间', '结算审计报告完成时间', '决算审计申请完成时间', '决算审计报告完成时间', \
     ]
-
-mapper = [(i + 2 if i != 0 else 0) for i in mapper0]
 
 class Project():
     def __init__(self, sheet):
@@ -38,36 +37,50 @@ class Project():
 
     def cell_value(self, row, col):
         value = self.sheet.cell(row, col).value
-        logging.info('(%s, %s) %s', row, col, value)
+        logging.debug('(%s, %s) %s', row, col, value)
         return value
 
     def parse(self):
+        logging.info('-------- Begin parsing sheet columns %s', self.sheet.name)
+        mapper = [0] * len(COL_NAMES)
+        for i in range(sheet.ncols):
+            col_name = self.cell_value(1, i)
+            if col_name in COL_NAMES:
+                index = COL_NAMES.index(col_name)
+                mapper[index] = i
+                logging.debug('found %s -> %s', i, col_name)
+        logging.info('-------- End parsing sheet columns %s', sheet.ncols)
+
+        for i, index in enumerate(mapper):
+            if index <= 0:
+                logging.warn('not found %s', COL_NAMES[i])
+
+        logging.info('-------- Begin reading sheet rows')
         for i in range(2, sheet.nrows):
             row = []
             for j in mapper:
-                row.append(self.cell_value(i, j))
+                if j > 0:
+                    row.append(self.cell_value(i, j))
+                else:
+                    row.append('')
             self.rows.append(row)
+        logging.info('-------- End reading sheet rows %s', sheet.nrows)
 
 INPUT_FILE_NAME = 'input/input.xlsx'
 input = xlrd.open_workbook(INPUT_FILE_NAME)
-logging.info('input file %s opend', INPUT_FILE_NAME)
+logging.info('input file %s opened', INPUT_FILE_NAME)
 
 sheet = input.sheets()[0]
-for i in range(1, sheet.ncols):
-    logging.info('%s : %s', i, sheet.cell(1, i))
-
-for j in mapper:
-    logging.info('%s : %s', j, sheet.cell(1, j))
-
-logging.info('-------- Begin reading sheet %s', sheet.name)
 p = Project(sheet)
 p.parse()
-logging.info('-------- End reading sheet %s', sheet.name)
 
 OUTPUT_FILE_NAME = 'output/output.csv'
 with open(OUTPUT_FILE_NAME, 'w', newline='') as output:
+    logging.info('output file %s opened', OUTPUT_FILE_NAME)
     writer = csv.writer(output)
+    logging.info('-------- Begin writing sheet rows')
     for r in p.rows:
         writer.writerow(r)
+    logging.info('-------- End writing sheet rows')
 
 logging.info('End PSR logging ...')
